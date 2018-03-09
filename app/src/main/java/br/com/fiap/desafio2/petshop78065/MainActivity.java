754b8e0bb8ffc42032ce1193915c2410c7b96a3a
logonrm <logonrm@fiap.com.br>
2018-03-08 22:09:54 -0300

    First commit

---
+++ app/src/main/java/br/com/fiap/desafio2/petshop78065/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.fiap.desafio2.petshop78065;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
